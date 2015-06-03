49bb412858b51ea7ddf8ff748a0c1b60897cd507
Deva <dev.palanisamy@gmail.com>
2015-06-03 15:21:14 +0100

    modified link with file

--- PayPalIosBinding/PayPalIosBinding/libPayPalMobile.linkwith.cs
+++ PayPalIosBinding/PayPalIosBinding/libPayPalMobile.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libPayPalMobile.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libPayPalMobile.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, Frameworks = "AVFoundation GLKit OpenGLES MobileCoreServices UIKit CoreVideo Foundation AudioToolbox CoreBluetooth CoreTelephony MediaPlayer SystemConfiguration QuartzCore CoreLocation CoreMedia MessageUI Accelerate", ForceLoad = true)]
