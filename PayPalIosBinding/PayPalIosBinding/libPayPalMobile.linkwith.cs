c3777658ded71814640d742cb4fc3470616086c6
Deva <dev.palanisamy@gmail.com>
2015-06-09 12:56:45 +0100

    Smart Link disabled

--- PayPalIosBinding/PayPalIosBinding/libPayPalMobile.linkwith.cs
+++ PayPalIosBinding/PayPalIosBinding/libPayPalMobile.linkwith.cs
@@ -1,4 +1,10 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libPayPalMobile.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, Frameworks = "AVFoundation GLKit OpenGLES MobileCoreServices UIKit CoreVideo Foundation AudioToolbox CoreBluetooth CoreTelephony MediaPlayer SystemConfiguration QuartzCore CoreLocation CoreMedia MessageUI Accelerate", ForceLoad = true)]
+[assembly: LinkWith ("libPayPalMobile.a", 
+	LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 |LinkTarget.Arm64, 
+	SmartLink = false, 
+	Frameworks = "AVFoundation GLKit OpenGLES MobileCoreServices UIKit CoreVideo Foundation AudioToolbox CoreBluetooth CoreTelephony MediaPlayer SystemConfiguration QuartzCore CoreLocation CoreMedia MessageUI Accelerate", 
+	ForceLoad = true, 
+	IsCxx = true, 
+	LinkerFlags="-lz -lxml2 -lc++ -lstdc++")]
